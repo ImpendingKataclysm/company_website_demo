25fd9711358d670baee805c666c8a7ea3a9cc448
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-13 15:51:36 -0700

    Create a database model for employees

--- main_site/admin.py
+++ main_site/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'last_name', 'first_name')
--- main_site/models.py
+++ main_site/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+MAX_FIELD_LEN = 80
+
+
+class Employee(models.Model):
+    """
+    Database table for company employees whose profiles are displayed on the
+    website.
+    """
+    first_name = models.CharField(max_length=MAX_FIELD_LEN)
+    last_name = models.CharField(max_length=MAX_FIELD_LEN)
+    role = models.CharField(max_length=MAX_FIELD_LEN)
+    image = models.ImageField(upload_to='employees')
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}: {self.role}"
