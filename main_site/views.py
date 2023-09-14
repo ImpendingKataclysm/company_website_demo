6fe5960c2269d6c82d0c5567179a94e2b8255e74
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-14 11:41:01 -0700

    Display employees from the database on the Team page

--- main_site/views.py
+++ main_site/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 from django.views import generic
+from . import models
 
 
 class HomePage(generic.TemplateView):
+    """
+    Display the home page
+    """
     template_name = 'main/index.html'
 
 
 class AboutPage(generic.TemplateView):
+    """
+    Display the about page
+    """
     template_name = 'main/about.html'
 
 
-class TeamPage(generic.TemplateView):
+class TeamPage(generic.ListView):
+    queryset = models.Employee.objects.order_by('last_name')
     template_name = 'main/team.html'
 
 
